133cf693d29c412d4a73e3d910a9c427bb6190fd
cem <cemhunerkar@gmail.com>
2017-03-19 20:09:55 +0300

    Light sensor added

--- app/src/main/java/cem/com/sensor/SensorActivity.java
+++ app/src/main/java/cem/com/sensor/SensorActivity.java
@@ -1,4 +1,5 @@
 package cem.com.sensor;
+
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -10,7 +11,8 @@ import android.widget.TextView;
 public class SensorActivity extends Activity implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mSensor;
-    TextView iv;
+    private Sensor lightSensor;
+    TextView iv, iv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +21,23 @@ public class SensorActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.sensor_screen);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         iv = (TextView) findViewById(R.id.jtext);
+        iv2 = (TextView) findViewById(R.id.jtext2);
     }
 
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mSensor,
                 SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, lightSensor,
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        System.exit(0);
     }
 
     protected void onPause() {
@@ -37,14 +49,22 @@ public class SensorActivity extends Activity implements SensorEventListener {
     }
 
     public void onSensorChanged(SensorEvent event) {
-        if (event.values[0] == 0) {
-            iv.setTextSize(50);
-            iv.setText("NEAR " + event.values[0]);
-            //System.out.println("near");
-        } else {
-            iv.setTextSize(50);
-            iv.setText("FAR " + event.values[0]);
-            //System.out.println("far");
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            iv2.setTextSize(50);
+            float f = event.values[0];
+            iv2.setText("LIGHT: " + ((int) f));
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            if (event.values[0] == 0) {
+                iv.setTextSize(50);
+                iv.setText("NEAR " + event.values[0]);
+                //System.out.println("near");
+            } else {
+                iv.setTextSize(50);
+                iv.setText("FAR " + event.values[0]);
+                //System.out.println("far");
+            }
         }
     }
 }
